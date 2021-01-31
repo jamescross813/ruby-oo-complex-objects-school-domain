7d89025c246f5eecc1534d0a29c18082896f1b90
jamescross <crossjames813@gmail.com>
2021-01-31 11:46:13 -0600

    commit tree 447a8845b88457d4e2d8b9df670cf94af31cd398

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,38 @@
 # code here!
+class School
+    
+    def initialize(name)
+        @roster = {}
+    end
+
+    def roster
+        @roster
+    end
+  
+    def add_student(name, grade)
+            if @roster.include?(grade)
+                @roster[grade] << name
+            else
+                @roster[grade] =[] 
+                @roster[grade] << name
+            end
+    end
+        
+    def grade(grade)
+            @roster.each do |grades, name|
+                if grades == grade 
+               @student_names = name
+            end
+        end
+    @student_names
+        end
+        
+    def sort
+            @roster.each do |grades, name|
+                name.sort!
+                end
+            end
+       
+
+end
+
